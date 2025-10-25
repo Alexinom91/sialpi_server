af0b272056968f7a84703f6cf9c8a569bd2d87fd
Alexinom91 <alessandro.scialpi@hotmail.com>
2025-10-25 23:33:12 +0200

    il server a cui mi sono connesso non accetta connessioni

--- user/user.c
+++ user/user.c
@@ -19,20 +19,34 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+//#include <stdlib.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
+#define PORT 8080
 
-#define PORT 2000
-#define IP_ADDR "46.62.213.203"
-int main(int argc, char **argv)
+int main()
 {
-    int socketFD=socket(AF_INET, SOCK_STREAM, NULL);
+    
+    int socketFD=socket(AF_INET, SOCK_STREAM, 0);
+    char *ip=" 	192.178.218.26";
     
     struct sockaddr_in address;
     address.sin_family=AF_INET;
-    address.sin_port=PORT;
-    address.sin_addr.s_addr=
-    connect(socketFD, &address, sizeof(address));
-    return EXIT_SUCCESS;
+    address.sin_port=htons(PORT);
+    inet_pton(AF_INET, ip, &address.sin_addr.s_addr);
+    printf("alert1\n");
+    int result=connect(socketFD,  (struct sockaddr*)&address, sizeof address);
+    printf("alert2\n");
+    if(result==0)
+    {
+        printf("The connection was successful\n");
+    }
+    else
+    {
+        printf("It wasnt sucessful %d, %d\n", errno, strerror(errno));
+    }
+    return 0;
 }
